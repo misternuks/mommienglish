0db5ef5c15c91483e7f1d482cc260bafd2f8eba2
Matthew Miner <misternuks@gmail.com>
2024-08-15 19:51:38 +0900

    add css classes, background image, and html components for the banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css';
+import { Zen_Maru_Gothic } from 'next/font/google';
+
+const zenMaruGothic = Zen_Maru_Gothic({
+  weight: ['400', '700'],
+  subsets: ['latin']
+});
+
 
 export const metadata = {
   title: 'MommiEnglish',
@@ -11,17 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={zenMaruGothic.className}>
       <body>
         <header>
-          <nav>
+          {/* <nav>
             <ul className = "navbar">
               <li><a href="/">Home</a></li>
               <li><a href="/about">About</a></li>
               <li><a href="/pricing">Pricing</a></li>
               <li><a href="/contact">Contact</a></li>
             </ul>
-          </nav>
+          </nav> */}
         </header>
         <main>{children}</main>
         <footer>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 export default function HomePage() {
   return (
-    <section>
-      <h1>ママが笑っていれば
-      それだけで嬉しい。</h1>
+    <section className = "banner">
+      <div className = "banner-hero">
+        <h1>
+          ママが<span className="red-text">笑</span>っていれば</h1>
+          <h1>それだけで<span className="red-text">嬉</span>しい。</h1>
+      <button className="red-button">
+        お問い合わせはこちら
+      </button>
+      </div>
+      <div >
+        <ul className="banner-buttons">
+          <li><a href="">HOME</a></li>
+          <li><a href="">サービス内容</a></li>
+          <li><a href="">料金</a></li>
+          <li><a href="">お問い合わせ</a></li>
+          <li><a href="">受講生専用PAGE</a></li>
+        </ul>
+      </div>
     </section>
   );
 }
