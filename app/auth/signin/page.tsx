d1cf6e3db7742fdda592fd4c577003b1aefef41b
Matthew Miner <misternuks@gmail.com>
2024-08-24 21:29:38 +0900

    create auth/signin CSS styles and move banner buttons to navbar

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,28 +1,36 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function SignIn() {
   return (
-    <div>
-      <h1>Sign In</h1>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        const target = e.target as typeof e.target & {
-          email: { value: string };
-          password: { value: string };
-        };
+    <div className="card-container">
+      <div className="card">
+        <Image
+          src='/images/logo_group.png'
+          alt='MommiEnglish logo and slogan group'
+          width={600}
+          height={150}
+        />
+        <form className="card-form" onSubmit={(e) => {
+          e.preventDefault();
+          const target = e.target as typeof e.target & {
+            email: { value: string };
+            password: { value: string };
+          };
 
-        signIn('credentials', {
-          redirect: false,
-          email: target.email.value,
-          password: target.password.value,
-        });
-      }}>
-        <input name="email" type="text" placeholder="Email" />
-        <input name="password" type="password" placeholder="Password" />
-        <button type="submit">Sign in</button>
-      </form>
+          signIn('credentials', {
+            redirect: false,
+            email: target.email.value,
+            password: target.password.value,
+          });
+        }}>
+          <input name="email" type="text" placeholder="Email" className="form-field"/>
+          <input name="password" type="password" placeholder="Password" className="form-field" />
+          <button type="submit" className="red-border-button" >Sign in</button>
+        </form>
+      </div>
     </div>
   );
 }
--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -25,15 +25,6 @@ export default function Banner() {
           </button>
         </div>
       </div>
-      <div>
-        <ul className= {styles['banner-buttons']} >
-          <li><a href='#mission'>Mission</a></li>
-          <li><a href='#service'>サービス内容</a></li>
-          <li><a href='#fee'>料金</a></li>
-          <li><a href='#contact'>お問い合わせ</a></li>
-          <li><a href='#student-page'>受講生専用PAGE</a></li>
-        </ul>
-      </div>
     </section>
   );
 }
