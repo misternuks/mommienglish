40d74713b2f8c59f578f674db93efeb17078955c
Matthew Miner <misternuks@gmail.com>
2024-09-11 23:53:12 +0900

    revert nextauth route and signin page to simple passcode

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,40 @@
 import NextAuth from 'next-auth';
-import { authOptions } from './authOptions'; // Import the authOptions
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+// import { PrismaClient, User } from '@prisma/client';
+// import bcrypt from 'bcrypt';
+
+// const prisma = new PrismaClient();
+
+const PASSCODE = process.env.PASSCODE;
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        passcode: { label: 'Passcode', type: 'password' },
+      },
+      authorize: async (credentials): Promise<any> => {
+        if (credentials?.passcode === PASSCODE) {
+          // Return a basic user object if matching
+          return { id: 1, name: 'User'};
+        }
+        // Return null if incorrect
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+
+// import NextAuth from 'next-auth';
+// import { authOptions } from './authOptions'; // Import the authOptions
+
+// const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST };
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,64 +1,128 @@
-// app/auth/signin/page.tsx
-"use client";
+'use client';
 
-import { signIn } from "next-auth/react";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import styles from "../../components/signin.module.css"
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import Image from 'next/image';
+import { useFlashMessage } from '@/app/context/FlashMessageContext';
 
 export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const { data: session, status } = useSession();
   const router = useRouter();
+  const { setFlashMessage } = useFlashMessage();
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  useEffect(()=> {
+    if (status === 'authenticated') {
+      router.push('/members'); // Redirect to the members pager if already authenticated
+    }
+  }, [status, router]);
+
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const target = e.target as typeof e.target & {
+      passcode: { value: string };
+    };
 
-    const result = await signIn("admin-login", {
+    const result = await signIn('credentials', {
       redirect: false,
-      email,
-      password,
+      passcode: target.passcode.value,
     });
 
-    if (result?.error) {
-      setError(result.error);
+    if (result?.ok) {
+      setFlashMessage({ type: 'success', message: 'Successfully signed in!' });
+      router.push('/members'); // Redirect to the members page after successful login
+    } else if (result?.error) {
+      setFlashMessage({ type: 'error', message: 'Incorrect passcode. Please try again.' });
     } else {
-      router.push("/admin"); // Redirect to admin dashboard upon success
+      setFlashMessage({ type: 'error', message: 'An unexpected error occurred. Please try again.' });
     }
   };
 
   return (
-    <div className={styles["login-container"]}>
-      <div className={styles["login-card"]}>
-        <h1>Admin Sign In</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <button className="red-button" type="submit">Sign In</button>
+    <div className="card-container">
+      <div className="card">
+        <Image
+          src='/images/logo_group.png'
+          alt='MommiEnglish logo and slogan group'
+          width={600}
+          height={150}
+        />
+        <form className="card-form" onSubmit={handleSignIn}>
+          <input name="passcode" type="password" placeholder="Passcode" className="form-field" />
+          <button type="submit" className="red-border-button" >Sign in</button>
         </form>
-
-        {/* Error message */}
-        {error && <p style={{ color: "red" }}>{error}</p>}
+        {/* <button
+          onClick={() => router.push('/auth/signup')}
+          className="red-button"
+        >
+          Sign up
+        </button> */}
       </div>
     </div>
   );
 }
+
+// // app/auth/signin/page.tsx for admin
+// "use client";
+
+// import { signIn } from "next-auth/react";
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
+// import styles from "../../components/signin.module.css"
+
+// export default function SignIn() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+//   const router = useRouter();
+
+//   const handleSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+
+//     const result = await signIn("admin-login", {
+//       redirect: false,
+//       email,
+//       password,
+//     });
+
+//     if (result?.error) {
+//       setError(result.error);
+//     } else {
+//       router.push("/admin"); // Redirect to admin dashboard upon success
+//     }
+//   };
+
+//   return (
+//     <div className={styles["login-container"]}>
+//       <div className={styles["login-card"]}>
+//         <h1>Admin Sign In</h1>
+//         <form onSubmit={handleSubmit}>
+//           <div>
+//             <label>Email</label>
+//             <input
+//               type="email"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//               required
+//             />
+//           </div>
+
+//           <div>
+//             <label>Password</label>
+//             <input
+//               type="password"
+//               value={password}
+//               onChange={(e) => setPassword(e.target.value)}
+//               required
+//             />
+//           </div>
+
+//           <button className="red-button" type="submit">Sign In</button>
+//         </form>
+
+//         {/* Error message */}
+//         {error && <p style={{ color: "red" }}>{error}</p>}
+//       </div>
+//     </div>
+//   );
+// }
